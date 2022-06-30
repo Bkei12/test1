b49705758ffa5ec1e747d8af56beacaa0f3c8a12
Bkei12 <rhqudrnr153@naver.com>
2022-06-30 15:52:13 +0900

    추가

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,59 +1,51 @@
 package com.example.myapplication;
 
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("unchecked")
+public class MainActivity<NavigationView> extends AppCompatActivity {
     Button btn1, btn2, btn3;  // 버튼 액션
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {   //상단바 생성
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {    //상단바 생성
-        switch (item.getItemId()){
-            case R.id.logout:
-                //select logout item
-                break;
-            case R.id.account:
-                //select account item
-                break;
-            case android.R.id.home:
-                //select back button
-                finish();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = (Button)findViewById(R.id.btn_1);
-        btn2 = (Button)findViewById(R.id.btn_2);
-        btn3 = (Button)findViewById(R.id.btn_3);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 왼쪽 상단 버튼 만들기
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24); //왼쪽 상단 버튼 아이콘 지정
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        //super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.btn_1);
+        btn2 = (Button) findViewById(R.id.btn_2);
+        btn3 = (Button) findViewById(R.id.btn_3);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 Fragment1 fragment1 = new Fragment1();
-                transaction.replace(R.id.frame,fragment1);
+                transaction.replace(R.id.frame, fragment1);
                 transaction.commit();
             }
         });
@@ -63,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 Fragment2 fragment2 = new Fragment2();
-                transaction.replace(R.id.frame,fragment2);
+                transaction.replace(R.id.frame, fragment2);
                 transaction.commit();
             }
         });
@@ -73,12 +65,34 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 Fragment3 fragment3 = new Fragment3();
-                transaction.replace(R.id.frame,fragment3);
+                transaction.replace(R.id.frame, fragment3);
                 transaction.commit();
             }
         });
 
     }
 
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() { //뒤로가기 했을 때
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
 }
